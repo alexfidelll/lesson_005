fe6342e6c4872cabacd7873293b655839086d8ad
fidel.alex <alekseifidel@gmail.com>
2023-08-26 23:07:55 +0500

    dz_27

---
+++ dz_27/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int Sum(int number)
+{
+    bool real = true;
+
+    int result = 0;
+
+    if (number < 0)
+    {
+        number = -number;
+    }
+    while (real)
+    {
+        if (number > 9)
+        {
+            result += number % 10;
+            number = number / 10;
+        }
+        else if (number < 10)
+        {
+            result += number;
+            real = false;
+        }
+    }
+    return result;
+}
+
+Console.WriteLine(Sum(452));
