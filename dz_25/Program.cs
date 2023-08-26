78b44b71a6ad76b5080d9b5a21b8eae9149b2e9a
fidel.alex <alekseifidel@gmail.com>
2023-08-26 22:18:32 +0500

    dz_25

---
+++ dz_25/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+int Pow(int firstValue, int secondValue)
+{
+     int result = firstValue;
+
+    for (int i = 1; i < secondValue; i++)
+    {
+        result *= firstValue;
+    }
+
+     return result;
+}
+
+Console.WriteLine(Pow(3, 5));
